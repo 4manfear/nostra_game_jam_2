bc6ea9df73f09992b3a0b7356f0aacba9eafb17b
4manfear <rickroad774@gmail.com>
2024-11-28 22:57:33 +0530

    m

---
+++ NostraGame/Assets/_Scripts/cat_evolve_checker/Cat_evolution_checker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat_evolution_checker : MonoBehaviour
+{
+    public bool cat_stage_1;
+    public bool cat_stage_2;
+    public bool cat_stage_3;
+}
