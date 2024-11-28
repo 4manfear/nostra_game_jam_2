9f0ca2e81e3c2229002c0dec435e43aad8f00441
4manfear <rickroad774@gmail.com>
2024-11-28 19:47:17 +0530

    dsf

--- NostraGame/Assets/_Scripts/PlayerControls/PlayerJump.cs
+++ NostraGame/Assets/_Scripts/PlayerControls/PlayerJump.cs
@@ -17,12 +17,21 @@ public class PlayerJump : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
     public void Jump()
     {
         if (isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             isGrounded = false; // Prevent double jumps
+            Debug.Log("jump");
         }
     }
 
