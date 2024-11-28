3e49e01e0300ae06d9a88e4c48fadd2981c9362b
4manfear <rickroad774@gmail.com>
2024-11-29 05:03:23 +0530

    all the mechanic is done

--- NostraGame/Assets/_Scripts/cat_evolve_system/food_counter.cs
+++ NostraGame/Assets/_Scripts/cat_evolve_system/food_counter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class food_counter : MonoBehaviour
 {
     public Slider evolve_slider;
--- NostraGame/Assets/_Scripts/coin_collection_script/Coin_Collection.cs
+++ NostraGame/Assets/_Scripts/coin_collection_script/Coin_Collection.cs
@@ -6,9 +6,11 @@ using TMPro;
 using UnityEngine.UI;
 
 public class Coin_Collection : MonoBehaviour
-{
+{// this script should be added on the coins 
+
+
     [SerializeField]
-    private Coins coin_scritable_object;
+    private Coins coin_scritable_object;// this is the scritable object
 
     
 
@@ -18,7 +20,7 @@ public class Coin_Collection : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Cat"))
+        if (collision.gameObject.CompareTag("Cat") || collision.gameObject.CompareTag("Player"))
         {
             coin_scritable_object.Dubloons++;
             Destroy(this.gameObject);
